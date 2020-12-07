d8ef97d814b1e13be3fa8ded583274a83cb839b7
alan.chen <alan.chen@unisoc.com>
2020-12-07 14:56:07 +0800

    Bug #1469258 misc: sprdwcn: fix crash of loopcheck fail when wcn pd is being processed
    [root cause  ] wcn visit ANLG_WRAP_WCN fail when wcn pd is being processed
    [changes     ] stop loopcheck before wcn pd
    [side effects] none
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: 9863a/9832e
    [test case]: power on/power down
    [reviewers   ] chaofan.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    Test: yes
    
    The patch fix crash of loopcheck fail when wcn pd is being processed
    
    Change-Id: I13069739238c3606a1d5e698f4401dd5a2a00cf4
    Signed-off-by: alan.chen <alan.chen@unisoc.com>

--- drivers/misc/sprdwcn/boot/wcn_integrate_boot.c
+++ drivers/misc/sprdwcn/boot/wcn_integrate_boot.c
@@ -860,6 +860,7 @@ int start_integrate_wcn_truely(u32 subsys)
 	if (is_marlin) {
 		mdbg_atcmd_clean();
 		wcn_set_module_state(true);
+		wcn_set_loopcheck_state(true);
 		marlin_bootup_time_update();
 		if (unlikely(!first_start)) {
 			wcn_firmware_init();
@@ -1012,6 +1013,8 @@ int stop_integrate_wcn_truely(u32 subsys)
 	}
 
 	WCN_INFO("%s do stop\n", wcn_dev->name);
+	if (is_marlin)
+		wcn_set_loopcheck_state(false);
 	/* btwf use the send shutdown cp2 cmd way */
 	if (is_marlin && !sprdwcn_bus_get_carddump_status())
 		force_sleep = wcn_send_force_sleep_cmd(wcn_dev);
